4126634a2b272059114963e7333b2a17fe094d0f
Elivelton S <samsepi0l.dev@gmail.com>
2023-03-21 21:45:48 -0300

    :label: add types for project

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,13 @@
+
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined
+      transaction: undefined
+      new: undefined
+      category: undefined
+      profile: undefined
+      wallet: undefined
+    }
+  }
+}
