f875e0cc3617b9b5baaa8c4b6bb1d99f1a039eb3
Elivelton S <samsepi0l.dev@gmail.com>
2023-03-27 01:53:09 -0300

    :sparkles: add lib for animation

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off'
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
   }
 }
